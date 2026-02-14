a7afd5db43e7b9dc163225dafd152d9000254f95
ouluc <mail2rameshchaudhary@gmail.com>
2026-02-14 15:52:20 +0530

    fixed KNN scaler issue

--- models/knn.py
+++ models/knn.py
@@ -1,6 +1,4 @@
 import joblib
 
 def load_model():
-    model = joblib.load("saved_models/knn.pkl")
-    scaler = joblib.load("saved_models/knn_scaler.pkl")
-    return model, scaler
+    return joblib.load("saved_models/knn.pkl")
